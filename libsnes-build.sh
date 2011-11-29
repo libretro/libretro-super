72764ebbd39a849ae560a5d1cabfaf265c2e0c26
TwinAphex51224 <twinaphex1@gmail.com>
2011-11-29 05:39:27 +0100

    Updated for FBA

--- libsnes-build.sh
+++ libsnes-build.sh
@@ -80,10 +80,10 @@ build_libsnes_fba()
 {
    if [ -d "libsnes-fba" ]; then
       echo "=== Building Final Burn Alpha ==="
-      cd libsnes-fba/src-0.2.97.13/burner/libsnes
-      make -j4 || die "Failed to build Final Burn Alpha"
-      cp libsnes.so ../../../libsnes-fba.so
-      cd ../../../..
+      cd libsnes-fba/
+      make -f Makefile.libsnes -j4 || die "Failed to build Final Burn Alpha"
+      cp libsnes.so libsnes-fba.so
+      cd ..
    else
       echo "Final Burn Alpha not fetched, skipping ..."
    fi
