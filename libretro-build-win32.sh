cb43c41f5ff9759b1415ec8a3e1ce1713d991805
Themaister <maister@archlinux.us>
2012-10-01 23:37:29 +0200

    Build on 0.2.97.27 FBA.

--- libretro-build-win32.sh
+++ libretro-build-win32.sh
@@ -84,8 +84,7 @@ build_libretro_fba()
 {
    if [ -d "libretro-fba" ]; then
       echo "=== Building Final Burn Alpha ==="
-      cd libretro-fba/src-0.2.97.26
-      make -f makefile.libretro generate-files
+      cd libretro-fba/src-0.2.97.27
       make CC=$CC CXX=$CXX platform=win -f makefile.libretro -j4 || die "Failed to build Final Burn Alpha"
       cp retro.dll ../libretro-git-fba.dll
       cd ../..
