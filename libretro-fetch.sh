e44a5be82e25cf05c968ac416afa477a71d72d4b
Themaister <maister@archlinux.us>
2012-11-08 23:55:59 +0100

    Fix fetch derp.

--- libretro-fetch.sh
+++ libretro-fetch.sh
@@ -90,5 +90,7 @@ fetch_project "$REPO_BASE/libretro/gambatte-libretro.git" "libretro-gambatte" "l
 fetch_project "$REPO_BASE/libretro/meteor-libretro.git" "libretro-meteor" "libretro/Meteor"
 fetch_project "$REPO_BASE/libretro/nxengine-libretro.git" "libretro-nx" "libretro/NX"
 fetch_project "$REPO_BASE/libretro/libretro-prboom.git" "libretro-prboom" "libretro/PRBoom"
+fetch_project "$REPO_BASE/libretro/stella-libretro.git" "libretro-stella" "libretro/Stella"
+fetch_project "$REPO_BASE/libretro/desmume-libretro.git" "libretro-desmume" "libretro/Desmume"
 fetch_project_mednafen "$REPO_BASE/libretro/mednafen-psx-libretro.git" "libretro-mednafen" "libretro/Mednafen"
 
