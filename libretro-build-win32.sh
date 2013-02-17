c3cc97e5b3096440187ea9238d7ec7bfb30f5844
Themaister <maister@archlinux.us>
2013-02-17 12:18:58 +0100

    More Win32 fixes.

--- libretro-build-win32.sh
+++ libretro-build-win32.sh
@@ -152,8 +152,8 @@ build_libretro_fceu()
    if [ -d "libretro-fceu" ]; then
       echo "=== Building FCEU ==="
       cd libretro-fceu
-      make CC=$CC CXX=$CXX platform=win -C src-fceumm -f Makefile.libretro -j4 || die "Failed to build FCEU"
-      cp src-fceumm/retro.dll libretro-git-fceu.dll
+      make CC=$CC CXX=$CXX platform=win -C fceumm-code -f Makefile.libretro -j4 || die "Failed to build FCEU"
+      cp fceumm-code/fceumm_retro.dll libretro-git-fceu.dll
       cd ..
    else
       echo "FCEU not fetched, skipping ..."
