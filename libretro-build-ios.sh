9e5056d047734fa215494e467a1e15c44aca1bc8
Jon Maddox <jon@jonmaddox.com>
2013-03-26 11:06:31 -0400

    ignore these for now until we get them building the right way

--- libretro-build-ios.sh
+++ libretro-build-ios.sh
@@ -253,14 +253,14 @@ build_libretro_mednafen
 build_libretro_s9x_next
 build_libretro_genplus
 build_libretro_fba
-build_libretro_vba
-build_libretro_fceu
+# build_libretro_vba
+# build_libretro_fceu
 build_libretro_gambatte
 build_libretro_nx
 build_libretro_prboom
 build_libretro_stella
 build_libretro_desmume
-build_libretro_quicknes
+# build_libretro_quicknes
 build_libretro_nestopia
 build_libretro_tyrquake
 
