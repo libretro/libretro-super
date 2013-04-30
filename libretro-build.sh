cd1d2eceb29808f4caf407a95b5d9215e2b4fecc
twinaphex <autechre1024@hotmail.com>
2013-04-30 03:26:46 +0200

    Fixup libretro-build.sh

--- libretro-build.sh
+++ libretro-build.sh
@@ -2,6 +2,7 @@
 
 SCRIPT=$(readlink -f $0)
 BASE_DIR=$(dirname $SCRIPT)
+JOBS=4
 
 die()
 {
@@ -19,7 +20,8 @@ build_libretro_bsnes()
    if [ -d "libretro-bsnes/perf" ]; then
       echo "=== Building bSNES performance ==="
       cd libretro-bsnes/perf/higan
-      make compiler="$CC" ui=target-libretro profile=performance -j4 || die "Failed to build bSNES performance core"
+      make compiler="$CC" ui=target-libretro profile=performance -j$JOBS clean || die "Failed to clean bSNES performance core"
+      make compiler="$CC" ui=target-libretro profile=performance -j$JOBS || die "Failed to build bSNES performance core"
       cp -f out/libretro.so ../../libretro-bsnes-performance.so
    else
       echo "bSNES performance not fetched, skipping ..."
@@ -29,7 +31,8 @@ build_libretro_bsnes()
    if [ -d "libretro-bsnes/balanced" ]; then
       echo "=== Building bSNES balanced ==="
       cd libretro-bsnes/balanced/higan
-      make compiler="$CC" ui=target-libretro profile=balanced -j4 || die "Failed to build bSNES balanced core"
+      make compiler="$CC" ui=target-libretro profile=balanced -j$JOBS clean || die "Failed to clean bSNES balanced core"
+      make compiler="$CC" ui=target-libretro profile=balanced -j$JOBS || die "Failed to build bSNES balanced core"
       cp -f out/libretro.so ../../libretro-bsnes-balanced.so
    else
       echo "bSNES compat not fetched, skipping ..."
@@ -39,7 +42,8 @@ build_libretro_bsnes()
    if [ -d "libretro-bsnes" ]; then
       echo "=== Building bSNES accuracy ==="
       cd libretro-bsnes/higan
-      make compiler="$CC" ui=target-libretro profile=accuracy -j4 || die "Failed to build bSNES accuracy core"
+      make compiler="$CC" ui=target-libretro profile=accuracy -j$JOBS clean || die "Failed to clean bSNES accuracy core"
+      make compiler="$CC" ui=target-libretro profile=accuracy -j$JOBS || die "Failed to build bSNES accuracy core"
       cp -f out/libretro.so ../libretro-bsnes-accuracy.so
    fi
 }
@@ -53,12 +57,9 @@ build_libretro_mednafen()
 
       for core in psx pce-fast wswan ngp gba snes vb
       do
-         if [ -d $core ]; then
-            cd $core
-            make core=${core} -j4 || die "Failed to build mednafen/${core}"
-            cp mednafen_$(echo ${core} | tr '[\-]' '[_]')_libretro.so ../libretro-mednafen-${core}.so
-            cd ..
-         fi
+         make core=${core} -j$JOBS clean || die "Failed to clean mednafen/${core}"
+         make core=${core} -j$JOBS || die "Failed to build mednafen/${core}"
+         cp mednafen_$(echo ${core} | tr '[\-]' '[_]')_libretro.so ../libretro-mednafen-${core}.so
       done
    else
       echo "Mednafen not fetched, skipping ..."
@@ -71,7 +72,8 @@ build_libretro_s9x()
    if [ -d "libretro-s9x" ]; then
       echo "=== Building SNES9x ==="
       cd libretro-s9x/libretro
-      make -j4 || die "Failed to build SNES9x"
+      make -j$JOBS clean || die "Failed to clean SNES9x"
+      make -j$JOBS || die "Failed to build SNES9x"
       cp libretro.so ../libretro-snes9x.so
    else
       echo "SNES9x not fetched, skipping ..."
@@ -84,7 +86,8 @@ build_libretro_s9x_next()
    if [ -d "libretro-s9x-next" ]; then
       echo "=== Building SNES9x-Next ==="
       cd libretro-s9x-next/
-      make -f Makefile.libretro -j4 || die "Failed to build SNES9x-Next"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean SNES9x-Next"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build SNES9x-Next"
       cp snes9x_next_libretro.so libretro-snes9x-next.so
    else
       echo "SNES9x-Next not fetched, skipping ..."
@@ -97,7 +100,8 @@ build_libretro_genplus()
    if [ -d "libretro-genplus" ]; then
       echo "=== Building Genplus GX ==="
       cd libretro-genplus/
-      make -f Makefile.libretro -j4 || die "Failed to build Genplus GX"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean Genplus GX"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build Genplus GX"
       cp genesis_plus_gx_libretro.so libretro-genplus.so
    else
       echo "Genplus GX not fetched, skipping ..."
@@ -110,8 +114,10 @@ build_libretro_fba()
    if [ -d "libretro-fba" ]; then
       echo "=== Building Final Burn Alpha ==="
       cd libretro-fba/
-      ./compile_libretro.sh make || die "Failed to build Final Burn Alpha"
-      cp svn-current/trunk/fb_alpha_libretro.so libretro-fba.so
+      cd svn-current/trunk
+      make -f makefile.libretro clean || die "Failed to clean Final Burn Alpha"
+      make -f makefile.libretro -j$JOBS || die "Failed to build Final Burn Alpha"
+      cp fb_alpha_libretro.so libretro-fba.so
    else
       echo "Final Burn Alpha not fetched, skipping ..."
    fi
@@ -123,7 +129,8 @@ build_libretro_vba()
    if [ -d "libretro-vba" ]; then
       echo "=== Building VBA-Next ==="
       cd libretro-vba/
-      make -f Makefile.libretro -j4 || die "Failed to build VBA-Next"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean VBA-Next"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build VBA-Next"
       cp vba_next_libretro.so libretro-vba.so
    else
       echo "VBA-Next not fetched, skipping ..."
@@ -137,7 +144,8 @@ build_libretro_bnes()
       echo "=== Building bNES ==="
       cd libretro-bnes
       mkdir -p obj
-      make -j4 || die "Failed to build bNES"
+      make -j$JOBS clean || die "Failed to clean bNES"
+      make -j$JOBS || die "Failed to build bNES"
       cp libretro.so libretro-bnes.so
    else
       echo "bNES not fetched, skipping ..."
@@ -150,7 +158,8 @@ build_libretro_fceu()
    if [ -d "libretro-fceu" ]; then
       echo "=== Building FCEU ==="
       cd libretro-fceu
-      make -C fceumm-code -f Makefile.libretro -j4 || die "Failed to build FCEU"
+      make -C fceumm-code -f Makefile.libretro -j$JOBS clean || die "Failed to clean FCEU"
+      make -C fceumm-code -f Makefile.libretro -j$JOBS || die "Failed to build FCEU"
       cp fceumm-code/fceumm_libretro.so libretro-fceu.so
    else
       echo "FCEU not fetched, skipping ..."
@@ -163,7 +172,8 @@ build_libretro_gambatte()
    if [ -d "libretro-gambatte" ]; then
       echo "=== Building Gambatte ==="
       cd libretro-gambatte/libgambatte
-      make -f Makefile.libretro -j4 || die "Failed to build Gambatte"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean Gambatte"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build Gambatte"
       cp gambatte_libretro.so ../libretro-gambatte.so
    else
       echo "Gambatte not fetched, skipping ..."
@@ -176,20 +186,21 @@ build_libretro_meteor()
    if [ -d "libretro-meteor" ]; then
       echo "=== Building Meteor ==="
       cd libretro-meteor/libretro
-      make -j4 || die "Failed to build Meteor"
+      make -j$JOBS clean || die "Failed to clean Meteor"
+      make -j$JOBS || die "Failed to build Meteor"
       cp libretro.so ../libretro-meteor.so
    else
       echo "Meteor not fetched, skipping ..."
    fi
 }
-
 build_libretro_nx()
 {
    cd $BASE_DIR
    if [ -d "libretro-nx" ]; then
       echo "=== Building NXEngine ==="
       cd libretro-nx
-      make -j4 || die "Failed to build NXEngine"
+      make -j$JOBS clean || die "Failed to clean NXEngine"
+      make -j$JOBS || die "Failed to build NXEngine"
       cp nxengine_libretro.so libretro-nx.so
    else
       echo "NXEngine not fetched, skipping ..."
@@ -202,7 +213,8 @@ build_libretro_prboom()
    if [ -d "libretro-prboom" ]; then
       echo "=== Building PRBoom ==="
       cd libretro-prboom
-      make -j4 || die "Failed to build PRBoom"
+      make -j$JOBS clean || die "Failed to clean PRBoom"
+      make -j$JOBS || die "Failed to build PRBoom"
       cp prboom_libretro.so libretro-prboom.so
    else
       echo "PRBoom not fetched, skipping ..."
@@ -215,7 +227,8 @@ build_libretro_stella()
    if [ -d "libretro-stella" ]; then
       echo "=== Building Stella ==="
       cd libretro-stella
-      make -j4 || die "Failed to build Stella"
+      make -j$JOBS clean || die "Failed to clean Stella"
+      make -j$JOBS  || die "Failed to build Stella"
       cp libretro.so libretro-stella.so
    else
       echo "Stella not fetched, skipping ..."
@@ -228,7 +241,8 @@ build_libretro_desmume()
    if [ -d "libretro-desmume" ]; then
       echo "=== Building Desmume ==="
       cd libretro-desmume
-      make -f Makefile.libretro -j4 || die "Failed to build Desmume"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean Desmume"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build Desmume"
       cp libretro.so libretro-desmume.so
    else
       echo "Desmume not fetched, skipping ..."
@@ -241,7 +255,8 @@ build_libretro_quicknes()
    if [ -d "libretro-quicknes" ]; then
       echo "=== Building QuickNES ==="
       cd libretro-quicknes/libretro
-      make -j4 || die "Failed to build QuickNES"
+      make -j$JOBS clean || die "Failed to clean QuickNES"
+      make -j$JOBS || die "Failed to build QuickNES"
       cp libretro.so ../libretro-quicknes.so
    else
       echo "QuickNES not fetched, skipping ..."
@@ -254,7 +269,8 @@ build_libretro_nestopia()
    if [ -d "libretro-nestopia" ]; then
       echo "=== Building Nestopia ==="
       cd libretro-nestopia/libretro
-      make -j4 || die "Failed to build Nestopia"
+      make -j$JOBS clean || die "Failed to clean Nestopia"
+      make -j$JOBS || die "Failed to build Nestopia"
       cp nestopia_libretro.so ../libretro-nestopia.so
    else
       echo "Nestopia not fetched, skipping ..."
@@ -267,7 +283,8 @@ build_libretro_tyrquake()
    if [ -d "libretro-tyrquake" ]; then
       echo "=== Building Tyr Quake ==="
       cd libretro-tyrquake
-      make -f Makefile.libretro -j4 || die "Failed to build Tyr Quake"
+      make -f Makefile.libretro -j$JOBS clean || die "Failed to clean Tyr Quake"
+      make -f Makefile.libretro -j$JOBS || die "Failed to build Tyr Quake"
       cp tyrquake_libretro.so libretro-tyrquake.so
    else
       echo "Tyr Quake not fetched, skipping ..."
