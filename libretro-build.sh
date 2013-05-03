5a9944ace3397d950ef54f84fd0c69656e6ccb95
twinaphex <autechre1024@hotmail.com>
2013-05-03 17:17:38 +0200

    Ability to pass function - allows you to compile one specific core instead of them all

--- libretro-build.sh
+++ libretro-build.sh
@@ -350,22 +350,26 @@ build_libretro_tyrquake()
 
 mkdir -p "$RARCH_DIST_DIR"
 
-build_libretro_desmume
-build_libretro_bsnes
-build_libretro_mednafen
-build_libretro_s9x
-build_libretro_s9x_next
-build_libretro_genplus
-build_libretro_fba
-build_libretro_vba
-build_libretro_bnes
-build_libretro_fceu
-build_libretro_gambatte
-build_libretro_meteor
-build_libretro_nx
-build_libretro_prboom
-build_libretro_stella
-build_libretro_quicknes
-build_libretro_nestopia
-build_libretro_tyrquake
-build_libretro_pcsx_rearmed
+if [ $1 ]; then
+   $1
+else
+   build_libretro_desmume
+   build_libretro_bsnes
+   build_libretro_mednafen
+   build_libretro_s9x
+   build_libretro_s9x_next
+   build_libretro_genplus
+   build_libretro_fba
+   build_libretro_vba
+   build_libretro_bnes
+   build_libretro_fceu
+   build_libretro_gambatte
+   build_libretro_meteor
+   build_libretro_nx
+   build_libretro_prboom
+   build_libretro_stella
+   build_libretro_quicknes
+   build_libretro_nestopia
+   build_libretro_tyrquake
+   build_libretro_pcsx_rearmed
+fi
