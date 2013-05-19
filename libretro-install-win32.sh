fecea617fd1fca36732c126af5387d825f5fcb59
Themaister <maister@archlinux.us>
2013-05-19 12:09:15 +0200

    Fixup install-win32.
    Uses date now as versioning scheme.

--- libretro-install-win32.sh
+++ libretro-install-win32.sh
@@ -5,13 +5,15 @@ ARCH_EXT="$1"
 SCRIPT=$(readlink -f "$0")
 BASE_DIR=$(dirname "$SCRIPT")
 RARCH_DIR="$BASE_DIR/dist"
-RARCH_DIST_DIR="$RARCH_DIR/windows"
+RARCH_DIST_DIR="$RARCH_DIR/win"
+
+DAY=$(date +"%Y%m%d")
 
 cd "$RARCH_DIST_DIR"
 for file in *.dll
 do
-   REGEX_MV="s|^\(.*\)\.dll$|\1-${ARCH_EXT}.dll|"
-   REGEX="s|^\(.*\)\.dll$|\1-${ARCH_EXT}.zip|"
+   REGEX_MV="s|^\(.*\)\.dll$|\1_${ARCH_EXT}_${DAY}.dll|"
+   REGEX="s|^\(.*\)\.dll$|\1_${ARCH_EXT}_${DAY}.zip|"
    FILENAME="`echo $file | sed -e $REGEX_MV`"
    mv -v "$file" "$FILENAME"
    zip "`echo $file | sed -e $REGEX`" "$FILENAME"
