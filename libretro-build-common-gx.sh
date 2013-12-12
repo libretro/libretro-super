b6e91e149791850e953e550883650e76398381b4
twinaphex <libretro@gmail.com>
2013-12-13 00:54:30 +0100

    (GX) Now builds recent CPS2 core

--- libretro-build-common-gx.sh
+++ libretro-build-common-gx.sh
@@ -21,13 +21,17 @@ build_libretro_fba()
       cd ../../
       
       echo "=== Building Final Burn Alpha Cores (CPS2) ==="
+      cd ../../
+      cd svn-current/trunk
       cd fbacores/cps2
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS2"
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS2"
-      cp libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT}
+      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT}
       cd ../../
 
       echo "=== Building Final Burn Alpha Cores (NeoGeo) ==="
+      cd ../../
+      cd svn-old/trunk
       cd fbacores/neogeo
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores NeoGeo"
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores NeoGeo"
