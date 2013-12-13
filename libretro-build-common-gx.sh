de0b5995d9d6d3f467fc0ca6144bc3f6926d10f4
twinaphex <libretro@gmail.com>
2013-12-13 03:08:27 +0100

    (GX) Rewrite libretro-build-common-gx.sh

--- libretro-build-common-gx.sh
+++ libretro-build-common-gx.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-build_libretro_fba()
+build_libretro_fba_cps1()
 {
    cd $BASE_DIR
    if [ -d "libretro-fba" ]; then
-      echo "=== Building Final Burn Alpha ==="
+      echo "=== Building Final Burn Alpha Cores (CPS1) ==="
       cd libretro-fba/
       cd svn-current/trunk
-      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha"
-      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha"
-      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR
-
-      echo "=== Building Final Burn Alpha Cores (CPS1) ==="
-      cd ../../
-      cd svn-old/trunk
       cd fbacores/cps1
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS1"
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS1"
       cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps1_libretro$FORMAT.${FORMAT_EXT}
-      cd ../../
-      
+   fi
+}
+
+build_libretro_fba_cps2()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-fba" ]; then
       echo "=== Building Final Burn Alpha Cores (CPS2) ==="
-      cd ../../
+      cd libretro-fba/
       cd svn-current/trunk
       cd fbacores/cps2
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS2"
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS2"
       cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT}
-      cd ../../
+   fi
+}
 
+build_libretro_fba_neogeo()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-fba" ]; then
       echo "=== Building Final Burn Alpha Cores (NeoGeo) ==="
-      cd ../../
+      cd libretro-fba/
       cd svn-old/trunk
       cd fbacores/neogeo
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores NeoGeo"
       make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores NeoGeo"
-      cp libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_neo_libretro$FORMAT.${FORMAT_EXT}
-   else
-      echo "Final Burn Alpha not fetched, skipping ..."
+      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_neo_libretro$FORMAT.${FORMAT_EXT}
    fi
+}
+
+build_libretro_fba()
+{
+   build_libretro_fba_full
+   build_libretro_fba_cps1
+   build_libretro_fba_cps2
+   build_libretro_fba_neogeo
 }
