90afbd48a3cd550ee86d31cbeb582259fb272751
twinaphex <libretro@gmail.com>
2013-12-15 21:02:19 +0100

    Add ScummVM compilation to libretro-build-ios.sh

--- libretro-build-ios.sh
+++ libretro-build-ios.sh
@@ -41,6 +41,7 @@ else
    build_libretro_nestopia
    build_libretro_tyrquake
    build_libretro_mame078
+   build_libretro_scummvm
    build_libretro_picodrive
    build_libretro_handy
    build_libretro_desmume
