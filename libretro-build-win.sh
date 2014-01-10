69f745db8d01a1950a4465de50cd8775c45a6119
twinaphex <autechre1024@hotmail.com>
2014-01-10 22:43:09 +0100

    Add libretro-build-win.sh

---
+++ libretro-build-win.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BASE_DIR=$(pwd)
+RARCH_DIR=$BASE_DIR/dist
+RARCH_DIST_DIR=$RARCH_DIR/win
+FORMAT=_win
+FORMAT_EXT=dll
+MSVC_NAME=msvc-2010
+
+die()
+{
+   echo $1
+   #exit 1
+}
+
+source $BASE_DIR/libretro-build-common-xdk.sh
+
+if [ $1 ]; then
+   $1
+else
+   #build_libretro_mednafen
+   #build_libretro_s9x_next
+   #build_libretro_genplus
+   #build_libretro_fba
+   #build_libretro_vba_next
+   #build_libretro_fceu
+   #build_libretro_gambatte
+   #build_libretro_nx
+   #build_libretro_prboom
+   #build_libretro_nestopia
+   build_libretro_tyrquake
+fi
