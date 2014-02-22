2ce07519ab2e61d27227d1f5ee265a150f4f5351
twinaphex <libretro@gmail.com>
2014-02-22 17:10:42 +0100

    Add libretro-build-psp1.sh

---
+++ libretro-build-psp1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f $0)
+BASE_DIR=$(dirname $SCRIPT)
+RARCH_DIR=$BASE_DIR/dist
+RARCH_DIST_DIR=$RARCH_DIR/psp1
+FORMAT=_psp1
+FORMAT_COMPILER_TARGET=psp1
+FORMAT_COMPILER_TARGET_ALT=psp1
+FORMAT_EXT=a
+JOBS=7
+MAKE=make
+
+. ./libretro-build-common.sh
+
+if [ $1 ]; then
+   $1
+else
+   #build_libretro_mednafen
+   build_libretro_s9x_next
+   build_libretro_genplus
+   #build_libretro_fba_full
+   build_libretro_vba_next
+   build_libretro_fceu
+   build_libretro_gambatte
+   build_libretro_nx
+   build_libretro_prboom
+   build_libretro_quicknes
+   build_libretro_nestopia
+   build_libretro_tyrquake
+   #build_libretro_mame078
+fi
