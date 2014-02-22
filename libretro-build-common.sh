8531ad2c06fdf118ec57668d5db4e432f8481c8f
twinaphex <libretro@gmail.com>
2014-02-22 23:15:13 +0100

    Add build_libretro_fba_cps2 to libretro-build-psp1.sh

--- libretro-build-common.sh
+++ libretro-build-common.sh
@@ -84,6 +84,48 @@ build_libretro_fba_full() {
    fi
 }
 
+build_libretro_fba_cps1()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-fba" ]; then
+      echo "=== Building Final Burn Alpha Cores (CPS1) ==="
+      cd libretro-fba/
+      cd svn-current/trunk
+      cd fbacores/cps1
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS1"
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS1"
+      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps1_libretro$FORMAT.${FORMAT_EXT}
+   fi
+}
+
+build_libretro_fba_cps2()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-fba" ]; then
+      echo "=== Building Final Burn Alpha Cores (CPS2) ==="
+      cd libretro-fba/
+      cd svn-current/trunk
+      cd fbacores/cps2
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS2"
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS2"
+      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT}
+   fi
+}
+
+build_libretro_fba_neogeo()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-fba" ]; then
+      echo "=== Building Final Burn Alpha Cores (NeoGeo) ==="
+      cd libretro-fba/
+      cd svn-current/trunk
+      cd fbacores/neogeo
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores NeoGeo"
+      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores NeoGeo"
+      cp fb_alpha_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_neo_libretro$FORMAT.${FORMAT_EXT}
+   fi
+}
+
 build_libretro_pcsx_rearmed() {
    cd "${BASE_DIR}"
    if [ -d 'libretro-pcsx-rearmed' ]; then
--- libretro-build-psp1.sh
+++ libretro-build-psp1.sh
@@ -20,6 +20,7 @@ else
    build_libretro_s9x_next
    build_libretro_genplus
    #build_libretro_fba_full
+   build_libretro_fba_cps2
    build_libretro_vba_next
    build_libretro_fceu
    build_libretro_gambatte
