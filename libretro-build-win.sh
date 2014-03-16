0f71b764de1e1c43108d5de482f071813199de0e
Twinaphex <libretro@gmail.com>
2014-03-16 16:48:03 +0100

    (MSVC) Add build_libretro_mednafen_gba

--- libretro-build-win.sh
+++ libretro-build-win.sh
@@ -21,6 +21,7 @@ if [ $1 ]; then
    $1
 else
    build_libretro_mednafen_pce_fast
+   build_libretro_mednafen_gba
    build_libretro_mednafen_ngp
    build_libretro_mednafen_vb
    build_libretro_mednafen_wswan
