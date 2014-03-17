bfc2c639f43fe38718e80e068d45e39a66a7aa3a
Twinaphex <libretro@gmail.com>
2014-03-17 13:31:11 +0100

    (MSVC) Update libretro-build-win.sh

--- libretro-build-common-xdk.sh
+++ libretro-build-common-xdk.sh
@@ -206,6 +206,20 @@ build_libretro_prboom()
    fi
 }
 
+build_libretro_stella()
+{
+   cd $BASE_DIR
+   if [ -d "libretro-stella" ]; then
+      echo "=== Building Stella ==="
+      cd libretro-stella
+      cd msvc
+      cmd.exe /k $MSVC_NAME.bat
+      cp $MSVC_NAME/$RELEASE_LTCG/${MSVC_NAME}.${FORMAT_EXT} "$RARCH_DIST_DIR"/stella_libretro$FORMAT.$FORMAT_EXT
+   else
+      echo "Stella not fetched, skipping ..."
+   fi
+}
+
 build_libretro_nestopia()
 {
    cd $BASE_DIR
