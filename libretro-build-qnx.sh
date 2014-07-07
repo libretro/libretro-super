7dd0991866f2083c4ebe751fee436177a90dcc15
twinaphex <libretro@gmail.com>
2014-07-07 22:54:21 +0200

    Add blueMSX and fMSX compilation to libretro-build-qnx.sh

--- libretro-build-qnx.sh
+++ libretro-build-qnx.sh
@@ -16,6 +16,8 @@ MAKE=make
 if [ $1 ]; then
    $1
 else
+   build_libretro_bluemsx
+   build_libretro_fmsx
    build_libretro_bsnes_cplusplus98
    #build_libretro_bsnes
    build_libretro_beetle_lynx
