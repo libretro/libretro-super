d2761f8f840de52f1f84708e6d106ce932c6df03
twinaphex <libretro@gmail.com>
2014-07-28 02:05:41 +0200

    Add libretro-joypad-autoconfig

--- libretro-fetch.sh
+++ libretro-fetch.sh
@@ -133,3 +133,4 @@ fetch_project "$REPO_BASE/libretro/blueMSX-libretro.git" "libretro-bluemsx" "lib
 fetch_project "$REPO_BASE/libretro/fmsx-libretro.git" "libretro-fmsx" "libretro/fmsx"
 fetch_project "$REPO_BASE/libretro/libretro-2048.git" "libretro-2048" "libretro/2048"
 fetch_project "$REPO_BASE/libretro/libretro-vecx.git" "libretro-vecx" "libretro/vecx"
+fetch_project "$REPO_BASE/libretro/retroarch-joypad-autoconfig.git" "libretro-joypad-autoconfig" "libretro/joypad-autoconfig"
