e19b270a01cac72311046017668c8745195142ee
twinaphex <libretro@gmail.com>
2014-08-15 02:20:17 +0200

    (PSP/QNX) Add o2em compilation to PSP/QNX

--- libretro-build-psp1.sh
+++ libretro-build-psp1.sh
@@ -48,4 +48,5 @@ else
    build_libretro_vecx
    build_libretro_tgbdual
    build_libretro_prosystem
+   build_libretro_o2em
 fi
--- libretro-build-qnx.sh
+++ libretro-build-qnx.sh
@@ -67,4 +67,5 @@ else
    build_libretro_tgbdual
    build_libretro_prosystem
    build_libretro_virtualjaguar
+   build_libretro_o2em
 fi
