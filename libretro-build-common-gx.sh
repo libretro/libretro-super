0820729d149b0640d97b262314a80eba33eb5423
twinaphex <libretro@gmail.com>
2014-08-22 15:51:04 +0200

    (GX) Refactor libretro_build_fba_cps2_gx

--- libretro-build-common-gx.sh
+++ libretro-build-common-gx.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-build_libretro_fba_cps2_gx()
-{
-   cd $BASE_DIR
-   if [ -d "libretro-fba" ]; then
-      echo "=== Building Final Burn Alpha Cores (CPS2) ==="
-      cd libretro-fba/
-      cd svn-old/trunk
-      cd fbacores/cps2
-      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS clean || die "Failed to clean Final Burn Alpha Cores CPS2"
-      make -f makefile.libretro platform=$FORMAT_COMPILER_TARGET -j$JOBS || die "Failed to build Final Burn Alpha Cores CPS2"
-      cp fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT} $RARCH_DIST_DIR/fba_cores_cps2_libretro$FORMAT.${FORMAT_EXT}
-   fi
-}
-
 build_libretro_fba()
 {
    build_libretro_fb_alpha
    build_libretro_fba_cps1
-   build_libretro_fba_cps2_gx
+   build_libretro_generic_makefile_subcore "fb_alpha" "fba_cores_cps2" "svn-old/trunk/fbacores/cps2" "makefile.libretro" ${FORMAT_COMPILER_TARGET}
    build_libretro_fba_neogeo
 }
