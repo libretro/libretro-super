1b15dca35ebc2276bd3ffaa47185e32dccdbb20e
twinaphex <libretro@gmail.com>
2014-12-24 02:11:31 +0100

    (Theos iOS) Add VBA and VBA next cores

--- libretro-build-ios-theos.sh
+++ libretro-build-ios-theos.sh
@@ -39,8 +39,8 @@ else
    build_libretro_snes9x_next
    build_libretro_genesis_plus_gx
    build_libretro_fb_alpha
-   #build_libretro_vbam
-   #build_libretro_vba_next
+   build_libretro_vbam
+   build_libretro_vba_next
    build_libretro_fceumm
    build_libretro_gambatte
    #build_libretro_meteor
