4f15f8960ccdf79a43c0618ce3609bad5403b6d6
Radius <radius@maildrop.cc>
2014-12-31 16:09:22 -0500

    bsnes buildbot build fixes

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 ####usage:
@@ -68,7 +69,12 @@ if [ -z "$RARCH_DIST_DIR" ]; then
     RARCH_DIST_DIR="$RARCH_DIR/$DIST_DIR"
 fi
 
-mkdir -p "$RARCH_DIST_DIR"
+if [ "${PURGE}" == "YES" ]; then
+   echo PURGE=$PURGE, cleaning up old cores
+   rm -rv "$RARCH_DIST_DIR"
+fi
+
+mkdir -v -p "$RARCH_DIST_DIR"
 
 if [ "${PLATFORM}" == "android" ];
 then
