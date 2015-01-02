622e08257d7f1fa98ad85981a40d6b5ae989c434
twinaphex <libretro@gmail.com>
2015-01-02 23:14:36 +0100

    Add blueMSX to libretro-build-android.mk.sh

--- libretro-build-android-mk.sh
+++ libretro-build-android-mk.sh
@@ -322,7 +322,7 @@ if [ $1 ]; then
 else
    build_libretro_2048
    build_libretro_4do
-   #build_libretro_bluemsx
+   build_libretro_bluemsx
    build_libretro_fmsx
    #build_libretro_bsnes_cplusplus98
    build_libretro_bsnes
