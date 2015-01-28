d6016ffb1584c177cfe379438ab62d0072352920
twinaphex <libretro@gmail.com>
2015-01-28 08:28:30 +0100

    Add ScummVM DAT to libretro-build-database.sh

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -157,6 +157,7 @@ build_libretro_database() {
 }
 
 build_libretro_databases() {
+   build_libretro_database "ScummVM" "rom.sha1"
    build_libretro_database "Nintendo - Super Nintendo Entertainment System" "rom.crc"
    build_libretro_database "Sony - PlayStation" "rom.serial"
    build_libretro_database "Atari - Jaguar" "rom.crc"
