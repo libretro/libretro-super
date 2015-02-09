3a19c26d22b88598624210e95c491218da53d423
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-02-08 16:16:00 -0800

    Begin modularizing the scripts

--- iKarith-libretro-fetch.sh
+++ iKarith-libretro-fetch.sh
@@ -11,38 +11,10 @@ else
 	BASE_DIR="$WORKDIR/$BASE_DIR"
 fi
 
-. $BASE_DIR/iKarith-libretro-config.sh
-
 WORKDIR=$(pwd)
 
-echo_cmd() {
-	echo "$@"
-	"$@"
-}
-
-
-# fetch_git <repository> <local directory>
-# Clones or pulls updates from a git repository into a local directory
-#
-# $1	The URI to fetch
-# $2	The local directory to fetch to (relative)
-# $3	Set to clone --recursive
-# $4	Set to pull --recursive
-fetch_git() {
-	fetch_dir="$WORKDIR/$2"
-	echo "=== Fetching $2 ==="
-	if [ -d "$fetch_dir/.git" ]; then
-		echo_cmd git -C "$fetch_dir" pull
-		if [ -n "$4" ]; then
-			echo_cmd git -C "$fetch_dir" submodule foreach git pull origin master
-		fi
-	else
-		echo_cmd git clone "$1" "$fetch_dir"
-		if [ -n "$3" ]; then
-			echo_cmd git -C "$fetch_dir" submodule update --init
-		fi
-	fi
-}
+. $BASE_DIR/iKarith-libretro-config.sh
+. $BASE_DIR/iKarith-super/fetch-rules.sh
 
 
 # Keep three copies so we don't have to rebuild stuff all the time.
