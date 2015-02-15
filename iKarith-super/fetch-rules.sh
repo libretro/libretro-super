9f46622c4437a9e64c000c063113982bd46c8ae1
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-02-15 11:06:52 -0800

    Add back pretty names—they were always coming back, but they're apparently needed immediately.

--- iKarith-super/fetch-rules.sh
+++ iKarith-super/fetch-rules.sh
@@ -12,23 +12,24 @@ echo_cmd() {
 #
 # $1	The URI to fetch
 # $2	The local directory to fetch to (relative)
-# $3	Set to clone --recursive
-# $4	Set to pull --recursive
+# $3  The pretty name for the === Fetching line ("" to print nothing)
+# $4	Set to clone --recursive
+# $5	Set to pull --recursive
 #
 # NOTE: git _now_ has a -C argument that would replace the cd commands in
 #       this rule, but this is a fairly recent addition to git, so we can't
 #       use it here.  --iKarith
 fetch_git() {
 	fetch_dir="$WORKDIR/$2"
-	echo "=== Fetching $2 ==="
+	[ -n "$3" ] && echo "=== Fetching $3 ==="
 	if [ -d "$fetch_dir/.git" ]; then
 		echo_cmd cd "$fetch_dir"
 		echo_cmd git pull
-		[ -n "$4" ] && echo_cmd git submodule foreach git pull origin master
+		[ -n "$5" ] && echo_cmd git submodule foreach git pull origin master
 	else
 		echo_cmd cd "$WORKDIR"
 		echo_cmd git clone "$1"
-		if [ -n "$3" ]; then
+		if [ -n "$4" ]; then
 			echo_cmd cd "$fetch_dir"
 			echo_cmd git submodule update --init
 		fi
