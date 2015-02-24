02e7339e342d7aa0a7e9dc688c2c2d7ced3c0b8a
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-02-23 16:08:05 -0800

    Fix a couple of rules

--- libretro-fetch.sh
+++ libretro-fetch.sh
@@ -30,7 +30,7 @@ libretro_snes9x_next_fetch_url="https://github.com/libretro/snes9x-next.git"
 libretro_snes9x_next_name="SNES9x Next"
 
 libretro_genesis_plus_gx_fetch_url="https://github.com/libretro/Genesis-Plus-GX.git"
-libretro_genesis_plus_gx="Genesis Plus GX"
+libretro_genesis_plus_gx_name="Genesis Plus GX"
 
 libretro_fb_alpha_fetch_url="https://github.com/libretro/fba-libretro.git"
 libretro_fb_alpha_name="Final Burn Alpha"
@@ -152,6 +152,7 @@ libretro_mupen64plus_fetch_url="https://github.com/libretro/mupen64plus-libretro
 libretro_mupen64plus_name="Mupen64Plus"
 
 libretro_dinothawr_fetch_url="https://github.com/libretro/Dinothawr.git"
+libretro_dinothawr_name="Dinothawr"
 
 libretro_uae_fetch_url="https://github.com/libretro/libretro-uae.git"
 libretro_uae_name="UAE"
