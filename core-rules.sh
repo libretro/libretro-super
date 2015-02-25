4b6033f3aac6e9f209b779ab292023591389ecfa
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-02-24 18:38:00 -0800

    Move core fetch rule variables to their own file

---
+++ core-rules.sh
@@ -0,0 +1,192 @@
+# vim: set ts=3 sw=3 noet ft=sh : bash
+
+
+libretro_bsnes_fetch_url="https://github.com/libretro/bsnes-libretro.git"
+libretro_bsnes_name="bsnes/higan"
+
+libretro_snes9x_fetch_url="https://github.com/libretro/snes9x.git"
+libretro_snes9x_name="SNES9x"
+
+libretro_snes9x_next_fetch_url="https://github.com/libretro/snes9x-next.git"
+libretro_snes9x_next_name="SNES9x Next"
+
+libretro_genesis_plus_gx_fetch_url="https://github.com/libretro/Genesis-Plus-GX.git"
+libretro_genesis_plus_gx_name="Genesis Plus GX"
+
+libretro_fb_alpha_fetch_url="https://github.com/libretro/fba-libretro.git"
+libretro_fb_alpha_name="Final Burn Alpha"
+
+libretro_vba_next_fetch_url="https://github.com/libretro/vba-next.git"
+libretro_vba_next_name="VBA Next"
+
+libretro_vbam_fetch_url="https://github.com/libretro/vbam-libretro.git"
+libretro_vbam_name="VBA-M"
+
+libretro_handy_fetch_url="https://github.com/libretro/libretro-handy.git"
+libretro_handy_name="Handy"
+
+libretro_bnes_fetch_url="https://github.com/libretro/bnes-libretro.git"
+libretro_bnes_name="bnes/higan"
+
+libretro_fceumm_fetch_url="https://github.com/libretro/libretro-fceumm.git"
+libretro_fceumm_name="FCEUmm"
+
+libretro_gambatte_fetch_url="https://github.com/libretro/gambatte-libretro.git"
+libretro_gambatte_name="Gambatte"
+
+libretro_meteor_fetch_url="https://github.com/libretro/meteor-libretro.git"
+libretro_meteor_name="Meteor"
+
+libretro_nxengine_fetch_url="https://github.com/libretro/nxengine-libretro.git"
+libretro_nxengine_name="NXEngine"
+
+libretro_prboom_fetch_url="https://github.com/libretro/libretro-prboom.git"
+libretro_prboom_name="PrBoom"
+
+libretro_stella_fetch_url="https://github.com/libretro/stella-libretro.git"
+libretro_stella_name="Stella"
+
+libretro_desmume_fetch_url="https://github.com/libretro/desmume.git"
+libretro_desmume_name="DeSmuME"
+
+libretro_quicknes_fetch_url="https://github.com/libretro/QuickNES_Core.git"
+libretro_quicknes_name="QuickNES"
+
+libretro_nestopia_fetch_url="https://github.com/libretro/nestopia.git"
+libretro_nestopia_name="Nestopia"
+
+libretro_tyrquake_fetch_url="https://github.com/libretro/tyrquake.git"
+libretro_tyrquake_name="TyrQuake"
+
+libretro_pcsx_rearmed_fetch_url="https://github.com/libretro/pcsx_rearmed.git"
+libretro_pcsx_rearmed_name="PCSX ReARMed"
+
+libretro_mednafen_gba_fetch_url="https://github.com/libretro/beetle-gba-libretro.git"
+libretro_mednafen_gba_name="Mednafen/Beetle GBA"
+
+libretro_mednafen_lynx_fetch_url="https://github.com/libretro/beetle-lynx-libretro.git"
+libretro_mednafen_lynx_name="Mednafen/Beetle Lynx"
+
+libretro_mednafen_ngp_fetch_url="https://github.com/libretro/beetle-ngp-libretro.git"
+libretro_mednafen_ngp_name="Mednafen/Beetle NeoPop"
+
+libretro_mednafen_pce_fast_fetch_url="https://github.com/libretro/beetle-pce-fast-libretro.git"
+libretro_mednafen_pce_fast_name="Mednafen/Beetle PCE FAST"
+
+libretro_mednafen_supergrafx_fetch_url="https://github.com/libretro/beetle-supergrafx-libretro.git"
+libretro_mednafen_supergrafx_name="Mednafen/Beetle SuperGrafx"
+
+libretro_mednafen_psx_fetch_url="https://github.com/libretro/mednafen-psx-libretro.git"
+libretro_mednafen_psx_name="Mednafen PSX"
+
+libretro_mednafen_pcfx_fetch_url="https://github.com/libretro/beetle-pcfx-libretro.git"
+libretro_mednafen_pcfx_name="Mednafen/Beetle PC-FX"
+
+libretro_mednafen_snes_fetch_url="https://github.com/libretro/beetle-bsnes-libretro.git"
+libretro_mednafen_snes_name="Mednafen/Beetle bsnes"
+
+libretro_mednafen_vb_fetch_url="https://github.com/libretro/beetle-vb-libretro.git"
+libretro_mednafen_vb_name="Mednafen/Beetle VB"
+
+libretro_mednafen_wswan_fetch_url="https://github.com/libretro/beetle-wswan-libretro.git"
+libretro_mednafen_wswan_name="Mednafen/Beetle WonderSwan"
+
+libretro_scummvm_fetch_url="https://github.com/libretro/scummvm.git"
+libretro_scummvm_name="ScummVM"
+
+libretro_yabause_fetch_url="https://github.com/libretro/yabause.git"
+libretro_yabause_name="Yabause"
+
+libretro_dosbox_fetch_url="https://github.com/libretro/dosbox-libretro.git"
+libretro_dosbox_name="DOSBox"
+
+libretro_virtualjaguar_fetch_url="https://github.com/libretro/virtualjaguar-libretro.git"
+libretro_virtualjaguar_name="Virtual Jaguar"
+
+libretro_mame078_fetch_url="https://github.com/libretro/mame2003-libretro.git"
+libretro_mame078_name="MAME 2003 (0.78)"
+
+libretro_mame139_fetch_url="https://github.com/libretro/mame2010-libretro.git"
+libretro_mame139_name="MAME 2010 (0.139)"
+
+libretro_mame_fetch_url="https://github.com/libretro/mame.git"
+libretro_mame_name="MAME (git)"
+
+libretro_ffmpeg_fetch_url="https://github.com/libretro/FFmpeg.git"
+libretro_ffmpeg_name="FFmpeg"
+
+libretro_bsnes_cplusplus98_fetch_url="https://github.com/libretro/bsnes-libretro-cplusplus98.git"
+libretro_bsnes_cplusplus98_name="bsnes C++98 (v0.85)"
+
+libretro_bsnes_mercury_fetch_url="https://github.com/libretro/bsnes-mercury.git"
+libretro_bsnes_mercury_name="bsnes-mercury"
+
+libretro_picodrive_fetch_url="https://github.com/libretro/picodrive.git"
+libretro_picodrive_name="Picodrive"
+libretro_picodrive_git_submodules="1"
+libretro_picodrive_git_submodules_update="1"
+
+libretro_tgbdual_fetch_url="https://github.com/libretro/tgbdual-libretro.git"
+libretro_tgbdual_name="TGB Dual"
+
+libretro_mupen64plus_fetch_url="https://github.com/libretro/mupen64plus-libretro.git"
+libretro_mupen64plus_name="Mupen64Plus"
+
+libretro_dinothawr_fetch_url="https://github.com/libretro/Dinothawr.git"
+libretro_dinothawr_name="Dinothawr"
+
+libretro_uae_fetch_url="https://github.com/libretro/libretro-uae.git"
+libretro_uae_name="UAE"
+
+libretro_3dengine_fetch_url="https://github.com/libretro/libretro-3dengine.git"
+libretro_3dengine_name="3DEngine"
+
+libretro_remotejoy_fetch_url="https://github.com/libretro/libretro-remotejoy.git"
+libretro_remotejoy_name="RemoteJoy"
+
+libretro_bluemsx_fetch_url="https://github.com/libretro/blueMSX-libretro.git"
+libretro_bluemsx_name="blueMSX"
+
+libretro_fmsx_fetch_url="https://github.com/libretro/fmsx-libretro.git"
+libretro_fmsx_name="fMSX"
+
+libretro_2048_fetch_url="https://github.com/libretro/libretro-2048.git"
+
+libretro_vecx_fetch_url="https://github.com/libretro/libretro-vecx.git"
+
+libretro_ppsspp_fetch_url="https://github.com/libretro/ppsspp.git"
+libretro_ppsspp_name="PPSSPP"
+libretro_ppsspp_git_submodules="1"
+libretro_ppsspp_git_submodules_update="1"
+
+libretro_prosystem_fetch_url="https://github.com/libretro/prosystem-libretro.git"
+libretro_prosystem_name="ProSystem"
+
+libretro_o2em_fetch_url="https://github.com/libretro/libretro-o2em.git"
+libretro_o2em_name="O2EM"
+
+libretro_4do_fetch_url="https://github.com/libretro/4do-libretro.git"
+libretro_4do_name="4DO"
+
+libretro_catsfc_fetch_url="https://github.com/libretro/CATSFC-libretro.git"
+libretro_catsfc_name="CATSFC"
+
+libretro_stonesoup_fetch_url="https://github.com/libretro/crawl-ref.git"
+libretro_stonesoup_name="Dungeon Crawl Stone Soup"
+libretro_stonesoup_git_submodules="1"
+
+libretro_hatari_fetch_url="https://github.com/libretro/hatari.git"
+libretro_hatari_name="Hatari"
+
+libretro_tempgba_fetch_url="https://github.com/libretro/TempGBA-libretro.git"
+libretro_tempgba_name="TempGBA"
+
+libretro_gpsp_fetch_url="https://github.com/libretro/gpsp.git"
+libretro_gpsp_name="gpSP"
+
+libretro_emux_fetch_url="https://github.com/libretro/emux.git"
+libretro_emux_name="Emux"
+
+libretro_fuse_fetch_url="https://github.com/libretro/fuse-libretro.git"
+libretro_fuse_name="Fuse"
+
