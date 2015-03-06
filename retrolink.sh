c0b3bc2ac481401f7669165f53c3af6cd0ea6a4b
Alcaro <Alcaro@users.noreply.github.com>
2015-03-07 00:49:44 +0100

    Update retrolink.sh

--- retrolink.sh
+++ retrolink.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #RetroLink - Allows a library or executable to link to any symbols, without version restrictions
 #Usage: ./retrolink foobar_libretro.so
+#http://www.lightofdawn.org/wiki/wiki.cgi/NewAppsOnOldGlibc
 
 start=$(readelf -V $1 | grep -A1 .gnu.version_r | tail -n1 | cut -d' ' -f6)
 pos=$(readelf -V $1 | grep 'Flags: none' | cut -d' ' -f3 | sed 's/://')
