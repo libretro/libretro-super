714dfc97f5c20f5b56ec9b5131603e87e06a1f72
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-03-24 10:49:46 -0700

    Remove an include that isn't ready

--- libretro-build-common.sh
+++ libretro-build-common.sh
@@ -4,7 +4,6 @@
 . "$BASE_DIR/script-modules/fetch-rules.sh"
 . "$BASE_DIR/script-modules/cpu.sh"
 . "$BASE_DIR/script-modules/modules.sh"
-. "$BASE_DIR/script-modules/build-tools.sh"
 
 . "$BASE_DIR/rules.d/core-rules.sh"
 
