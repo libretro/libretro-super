571e23d655054597a52498a867f63a4780a578f0
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-04-12 14:56:10 -0700

    Move module processing to a script module

--- libretro-build-common.sh
+++ libretro-build-common.sh
@@ -4,7 +4,7 @@
 . "$BASE_DIR/script-modules/util.sh"
 . "$BASE_DIR/script-modules/fetch-rules.sh"
 . "$BASE_DIR/script-modules/cpu.sh"
-. "$BASE_DIR/script-modules/modules.sh"
+. "$BASE_DIR/script-modules/module_base.sh"
 
 . "$BASE_DIR/rules.d/core-rules.sh"
 
