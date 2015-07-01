daa57b6315fa3a68e8d87e4542cbce87619c95e1
T. Joseph Carter <iKarith@users.noreply.github.com>
2015-07-01 15:22:26 -0700

    Correct module registration line for retroarch

--- rules.d/player-rules.sh
+++ rules.d/player-rules.sh
@@ -1,6 +1,6 @@
 # vim: set ts=3 sw=3 noet ft=sh : bash
 
-register_module player any
+register_module player retroarch
 libretro_retroarch_name="RetroArch"
 libretro_retroarch_dir="retroarch"
 libretro_retroarch_git_url="https://github.com/libretro/RetroArch.git"
