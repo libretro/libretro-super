cea56373b77e210e7b8a0673cfaa6368bea1b7a6
Radius <radius@maildrop.cc>
2015-07-04 16:57:26 -0500

    remove databases on psp build

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -1388,13 +1388,13 @@ if [ "${PLATFORM}" = "psp1" ] && [ "${RA}" = "YES" ]; then
 
 			mkdir -p psp1/pkg/
 			mkdir -p psp1/pkg/cheats
-			mkdir -p psp1/pkg/database
-			mkdir -p psp1/pkg/database/cursors
+#			mkdir -p psp1/pkg/database
+#			mkdir -p psp1/pkg/database/cursors
 			mkdir -p psp1/pkg/database/rdb
 
-			cp -Rfv media/libretrodb/cht/* psp1/pkg/cheats
-			cp -Rfv media/libretrodb/rdb/* psp1/pkg/database/rdb
-			cp -Rfv media/libretrodb/cursors/* psp1/pkg/database/cursors
+#			cp -Rfv media/libretrodb/cht/* psp1/pkg/cheats
+#			cp -Rfv media/libretrodb/rdb/* psp1/pkg/database/rdb
+#			cp -Rfv media/libretrodb/cursors/* psp1/pkg/database/cursors
 		fi
 	fi
 fi
