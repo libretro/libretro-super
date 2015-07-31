d90ea5d4eab148da76e0487ff4f89bf29ea39c53
twinaphex <libretro@gmail.com>
2015-07-31 05:05:01 +0200

    Modify build rules

--- libretro-build.sh
+++ libretro-build.sh
@@ -94,29 +94,23 @@ build_default_cores() {
 	# (They also use rules builds, which will help later)
 
 	libretro_build_core 2048
-	libretro_build_core 3dengine
-	libretro_build_core 4do
 	libretro_build_core bluemsx
 	libretro_build_core catsfc
-	libretro_build_core desmume
 	libretro_build_core dosbox
 	libretro_build_core fb_alpha
 	libretro_build_core fceumm
 	libretro_build_core fmsx
 	libretro_build_core gambatte
-	libretro_build_core gpsp
 	libretro_build_core handy
 	libretro_build_core meteor
 	libretro_build_core nestopia
 	libretro_build_core nxengine
-	libretro_build_core o2em
 	libretro_build_core prboom
 	libretro_build_core prosystem
 	libretro_build_core quicknes
 	libretro_build_core snes9x
 	libretro_build_core snes9x_next
 	libretro_build_core stella
-	libretro_build_core tgbdual
 	libretro_build_core tyrquake
 	libretro_build_core vba_next
 	libretro_build_core vbam
@@ -126,17 +120,13 @@ build_default_cores() {
 	# Nothing past here supports theos
 	[ "$platform" = "theos_ios" ] && return
 
-	libretro_build_core bsnes
 	libretro_build_core bsnes_cplusplus98
-	libretro_build_core bsnes_mercury
 
 	libretro_build_core emux
 	libretro_build_core fuse
 	libretro_build_core genesis_plus_gx
-	libretro_build_core gw
 	libretro_build_core hatari
 	libretro_build_core lutro
-	libretro_build_core mame
 	libretro_build_core mame078
 	libretro_build_core mednafen_gba
 	libretro_build_core mednafen_lynx
@@ -148,18 +138,50 @@ build_default_cores() {
 	libretro_build_core mednafen_supergrafx
 	libretro_build_core mednafen_vb
 	libretro_build_core mednafen_wswan
-	libretro_build_core mupen64plus
-	libretro_build_core picodrive
-	libretro_build_core scummvm
-	libretro_build_core yabause
 
 	if [ $platform != "qnx" ]; then
-		# C++11 cores - not supported yet for these targets
+		libretro_build_core gw
+	fi
+
+	if [ $platform != "ps3" ] && [ $platform != "sncps3" ] && [ $platform != "wii" ] && [ $platform != "ngc" ]; then
+		# These cores might not yet be big-endian compatible
+		libretro_build_core picodrive
+		libretro_build_core tgbdual
+		libretro_build_core gpsp
+		libretro_build_core o2em
+		libretro_build_core 4do
+		libretro_build_core desmume
+
+		# Reasons for not compiling this yet on these targets (other than endianness issues)
+		# 1) Wii/NGC - no PPC dynarec, no usable graphics plugins that work with GX
+		# 2) PS3     - no PPC dynarec, PSGL is GLES 1.0 while graphics plugins right now require GL 2.0+/GLES2
+		libretro_build_core mupen64plus
+
+		# Graphics require GLES 2/GL 2.0
+		libretro_build_core 3dengine
+
+		# The only reason this won't be compiled in yet for PS3/Wii is
+		# 1) Wii/NGC - too big in binary size
+		# 2) PS3 - filesystem API issues
+		libretro_build_core scummvm
+
+		# Could work on PS3/Wii right now but way too slow right now,
+		# and messed up big-endian colors
+		libretro_build_core yabause
+	fi
+
+	if [ $platform != "qnx" && [ $platform != "ps3" ] && [ $platform != "sncps3" ]; then
+		# These are C++11 cores - not supported by these targets
 		libretro_build_core dinothawr
 		libretro_build_core stonesoup
+		libretro_build_core bsnes
+		libretro_build_core bsnes_mercury
+		libretro_build_core mame
 	fi
 
 	if [ $platform != "win" ]; then
+		# Reasons for not compiling this on Windows yet -
+		# (Windows) - Doesn't work properly
 		libretro_build_core pcsx_rearmed
 	fi
 
