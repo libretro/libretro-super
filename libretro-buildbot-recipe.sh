500227b36fe6edf18c144096c752b8cfe5fef003
Radius <andres.430@gmail.com>
2015-08-13 23:11:51 +0100

    update buildbot script

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -260,6 +260,8 @@ build_libretro_leiradel_makefile() {
 	PLATFORM=$5
 	ARGS=$6
 
+        ARG1=`echo ${ARGS} | cut -f 1 -d " "`
+
 	cd $DIR
 	cd $SUBDIR
 	OLDJ=$JOBS
@@ -281,7 +283,8 @@ build_libretro_leiradel_makefile() {
 
 		if [ $? -eq 0 ]; then
 			MESSAGE="$1 build successful [$jobid]"
-              		cp -v ${NAME}_libretro.${PLATFORM}-${ARGS}.${FORMAT_EXT} $RARCH_DIST_DIR/${DIST}/${ARGS}/${NAME}_libretro${SUFFIX}.${FORMAT_EXT}  &>> /tmp/log/${BOT}/${LOGDATE}/${LOGDATE}_${NAME}_${PLATFORM}.log
+              		echo "cp -v ${NAME}_libretro.${PLATFORM}-${ARG1}.${FORMAT_EXT} $RARCH_DIST_DIR/${DIST}/${ARG1}/${NAME}_libretro${SUFFIX}.${FORMAT_EXT}"
+              		cp -v ${NAME}_libretro.${PLATFORM}-${ARG1}.${FORMAT_EXT} $RARCH_DIST_DIR/${DIST}/${ARGS}/${NAME}_libretro${SUFFIX}.${FORMAT_EXT}  &>> /tmp/log/${BOT}/${LOGDATE}/${LOGDATE}_${NAME}_${PLATFORM}.log
 		else
 		ERROR=`cat /tmp/log/${BOT}/${LOGDATE}/${LOGDATE}_${NAME}_${PLATFORM}.log | tail -n 100`
 		HASTE=`curl -XPOST http://hastebin.com/documents -d"$ERROR" | cut --fields=4 --delimiter='"'`
