9dc71e8dba712b66ac5453107c531e8edca4c329
radius <andres.430@gmail.com>
2015-10-21 10:35:27 -0500

    update RA 3ds logging

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -1801,14 +1801,12 @@ if [ "${PLATFORM}" == "ctr" ] && [ "${RA}" == "YES" ]; then
 		cp -v $RARCH_DIST_DIR/*.a .
 
 		#ls -1 *.a  | awk -F "." ' { print "cp " $0 " " $1 "_ctr." $2 }' |sh
-		JOBS=1 sh ./dist-cores.sh ctr 2>&1 retroarch-ctr.log
+		JOBS=1 sh ./dist-cores.sh ctr 2>&1 $TMPDIR/log/${BOT}/${LOGDATE}/${LOGDATE}_RetroArch_${PLATFORM}.log
 		if [ $? -eq 0 ]; then
 			MESSAGE="retroarch build succeeded [$jobid]"
 			echo $MESSAGE
-			cat retroarch-ctr.log > $TMPDIR/log/${BOT}/${LOGDATE}/${LOGDATE}_RetroArch_${PLATFORM}.log
 			touch $TMPDIR/built-frontend-3ds
 		else
-			cat retroarch-ctr.log > $TMPDIR/log/${BOT}/${LOGDATE}/${LOGDATE}_RetroArch_${PLATFORM}.log
 			ERROR=`cat $TMPDIR/log/${BOT}/${LOGDATE}/${LOGDATE}_RetroArch_${PLATFORM}.log | tail -n 100`
 			HASTE=`curl -XPOST http://hastebin.com/documents -d"$ERROR"`
 			HASTE=`echo $HASTE | cut -d"\"" -f4`
