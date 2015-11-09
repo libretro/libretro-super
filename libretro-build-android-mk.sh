53f9c34482774a7200e40b5b8d213c0b80afca74
twinaphex <libretro@gmail.com>
2015-11-09 03:24:41 +0100

    (android-mk.sh) Cut down on long build rules

--- libretro-build-android-mk.sh
+++ libretro-build-android-mk.sh
@@ -62,38 +62,6 @@ build_libretro_generic_makefile()
 	fi
 }
 
-build_libretro_2048() {
-	build_libretro_generic_makefile "2048" "jni"
-}
-
-build_libretro_4do() {
-	build_libretro_generic_makefile "4do" "jni"
-}
-
-build_libretro_bluemsx() {
-	build_libretro_generic_makefile "bluemsx" "jni"
-}
-
-build_libretro_fmsx() {
-	build_libretro_generic_makefile "fmsx" "jni"
-}
-
-build_libretro_dosbox() {
-	build_libretro_generic_makefile "dosbox" "jni"
-}
-
-build_libretro_stella() {
-	build_libretro_generic_makefile "stella" "jni"
-}
-
-build_libretro_genesis_plus_gx() {
-	build_libretro_generic_makefile "genesis_plus_gx" "libretro/jni"
-}
-
-build_libretro_gpsp() {
-	build_libretro_generic_makefile "gpsp" "jni"
-}
-
 build_libretro_vba_next() {
 	build_libretro_generic_makefile "vba_next" "libretro/jni"
 }
@@ -102,68 +70,9 @@ build_libretro_vbam() {
 	build_libretro_generic_makefile "vbam" "src/libretro/jni"
 }
 
-build_libretro_catsfc() {
-	build_libretro_generic_makefile "catsfc" "jni"
-}
-
-build_libretro_prosystem() {
-	build_libretro_generic_makefile "prosystem" "jni"
-}
 
 build_libretro_tgbdual() {
 	build_libretro_generic_makefile "tgbdual" "libretro/jni"
-}
-
-build_libretro_snes9x() {
-	build_libretro_generic_makefile "snes9x" "libretro/jni"
-}
-
-build_libretro_snes9x_next() {
-	build_libretro_generic_makefile "snes9x_next" "libretro/jni"
-}
-
-build_libretro_beetle_bsnes() {
-	build_libretro_generic_makefile "mednafen_snes" "jni"
-}
-
-build_libretro_beetle_lynx() {
-	build_libretro_generic_makefile "mednafen_lynx" "jni"
-}
-
-build_libretro_beetle_gba() {
-	build_libretro_generic_makefile "mednafen_gba" "jni"
-}
-
-build_libretro_beetle_ngp() {
-	build_libretro_generic_makefile "mednafen_ngp" "jni"
-}
-
-build_libretro_beetle_wswan() {
-	build_libretro_generic_makefile "mednafen_wswan" "jni"
-}
-
-build_libretro_beetle_psx() {
-	build_libretro_generic_makefile "mednafen_psx" "jni"
-}
-
-build_libretro_beetle_pcfx() {
-	build_libretro_generic_makefile "mednafen_pcfx" "jni"
-}
-
-build_libretro_beetle_vb() {
-	build_libretro_generic_makefile "mednafen_vb" "jni"
-}
-
-build_libretro_beetle_pce_fast() {
-	build_libretro_generic_makefile "mednafen_pce_fast" "jni"
-}
-
-build_libretro_beetle_supergrafx() {
-	build_libretro_generic_makefile "mednafen_supergrafx" "jni"
-}
-
-build_libretro_nx() {
-	build_libretro_generic_makefile "nxengine" "jni"
 }
 
 build_libretro_prboom()
@@ -179,16 +88,8 @@ build_libretro_tyrquake() {
 	build_libretro_generic_makefile "tyrquake" "libretro/jni"
 }
 
-build_libretro_pcsx_rearmed() {
-	build_libretro_generic_makefile "pcsx_rearmed" "jni"
-}
-
 build_libretro_ppsspp() {
 	build_libretro_generic_makefile "ppsspp" "libretro/jni"
-}
-
-build_libretro_picodrive() {
-	build_libretro_generic_makefile "picodrive" "jni"
 }
 
 build_libretro_quicknes() {
@@ -212,13 +113,6 @@ build_libretro_mupen64()
 	build_libretro_generic_makefile "mupen64plus" "libretro/jni"
 }
 
-build_libretro_3dengine() {
-	build_libretro_generic_makefile "3dengine" "jni"
-}
-
-build_libretro_o2em() {
-	build_libretro_generic_makefile "o2em" "jni"
-}
 
 build_libretro_fceumm() {
 	build_libretro_generic_makefile "fceumm" "src/drivers/libretro/jni"
@@ -228,18 +122,12 @@ build_libretro_gambatte() {
 	build_libretro_generic_makefile "gambatte" "libgambatte/libretro/jni"
 }
 
-build_libretro_meteor() {
-	build_libretro_generic_makefile "meteor" "libretro/jni"
-}
 
 
 build_libretro_dinothawr() {
 	build_libretro_generic_makefile "dinothawr" "android/eclipse/jni"
 }
 
-build_libretro_virtualjaguar() {
-	build_libretro_generic_makefile "virtualjaguar" "jni"
-}
 
 build_libretro_desmume() {
 	build_libretro_generic_makefile "desmume" "desmume/src/libretro/jni"
@@ -319,60 +207,45 @@ build_libretro_bsnes_mercury()
 create_dist_dir
 
 if [ $1 ]; then
-	$1
+	WANT_CORES="$1"
 else
-	build_libretro_2048
-	build_libretro_4do
-	build_libretro_bluemsx
-	build_libretro_fmsx
-	#build_libretro_bsnes_cplusplus98
-	build_libretro_bsnes
-	build_libretro_bsnes_mercury
-	build_libretro_beetle_lynx
-	#build_libretro_beetle_gba
-	build_libretro_beetle_ngp
-	build_libretro_beetle_pce_fast
-	build_libretro_beetle_supergrafx
-	build_libretro_beetle_pcfx
-	build_libretro_beetle_vb
-	build_libretro_beetle_wswan
-	build_libretro_mednafen_psx
-	#build_libretro_beetle_bsnes
-	build_libretro_catsfc
-	build_libretro_snes9x
-	build_libretro_snes9x_next
-	build_libretro_genesis_plus_gx
-	build_libretro_fb_alpha
-	build_libretro_vbam
-	build_libretro_vba_next
-	#build_libretro_bnes
-	build_libretro_fceumm
-	build_libretro_gambatte
-	build_libretro_meteor
-	build_libretro_nx
-	build_libretro_prboom
-	build_libretro_stella
-	build_libretro_quicknes
-	build_libretro_nestopia
-	build_libretro_tyrquake
-	#build_libretro_mame078
-	#build_libretro_mame
-	build_libretro_dosbox
-	#build_libretro_scummvm
-	build_libretro_picodrive
-	build_libretro_handy
-	build_libretro_desmume
-	build_libretro_pcsx_rearmed
-	build_libretro_yabause
-	build_libretro_vecx
-	build_libretro_tgbdual
-	build_libretro_prosystem
-	build_libretro_dinothawr
-	build_libretro_virtualjaguar
-	build_libretro_mupen64
-	#build_libretro_ffmpeg
-	build_libretro_3dengine
-	#build_libretro_ppsspp
-	build_libretro_o2em
-	build_libretro_gpsp
+WANT_CORES=" \
+	2048 \
+   4do \
+	bluemsx \
+	fmsx \
+	mednafen_lynx \
+	mednafen_ngp \
+	mednafen_pce_fast \
+	mednafen_supergrafx \
+	mednafen_pcfx \
+	mednafen_vb \
+	mednafen_wswan \
+	mednafen_psx \
+	catsfc \
+	snes9x \
+	snes9x_next \
+	genesis_plus_gx \
+	virtualjaguar \
+	stella \
+	gpsp \
+	dosbox \
+	picodrive \
+	3dengine \
+	prosystem \
+	meteor \
+	nxengine \
+	o2em \
+	pcsx_rearmed"
+build_libretro_bsnes
+build_libretro_bsnes_mercury
 fi
+
+for core in $WANT_CORES; do
+	path="jni"
+	if [ $core = "snes9x" ] || [ $core = "snes9x_next" ] || [ $core = "genesis_plus_gx" ] || [ $core = "meteor" ]; then
+		path="libretro/jni"
+	fi
+	build_libretro_generic_makefile $core $path
+done
+
