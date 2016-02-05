654c0f203ce4689fd41343caf32144286e4dae2c
twinaphex <libretro@gmail.com>
2016-02-05 17:27:12 +0100

    Add cores to libretro-build-static.sh

---
+++ libretro-build-static.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+# vim: set ts=3 sw=3 noet ft=sh : bash
+
+SCRIPT="${0#./}"
+BASE_DIR="${SCRIPT%/*}"
+WORKDIR="$PWD"
+
+if [ "$BASE_DIR" = "$SCRIPT" ]; then
+	BASE_DIR="$WORKDIR"
+else
+	if [[ "$0" != /* ]]; then
+		# Make the path absolute
+		BASE_DIR="$WORKDIR/$BASE_DIR"
+	fi
+fi
+
+if [[ -z "$1" ]]; then
+WANT_CORES=" \
+	vba_next \
+	2048 \
+	prboom \
+	reicast \
+	snes9x_next"
+else
+WANT_CORES="$@"
+fi
+
+export STATIC_LINKING=1
+
+${BASE_DIR}/libretro-build.sh ${WANT_CORES}
