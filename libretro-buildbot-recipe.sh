dab625666ec902a9079bac81934e644b8e04ef68
radius <andres.430@gmail.com>
2016-03-23 12:42:31 -0500

    fix last commit

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -1721,8 +1721,8 @@ if [ "${PLATFORM}" == "ctr" ] && [ "${RA}" == "YES" ]; then
 		mkdir -p $WORK/$RADIR/pkg/3ds/retroarch/filters
 		mkdir -p $WORK/$RADIR/pkg/3ds/retroarch/filters/audio
 		mkdir -p $WORK/$RADIR/pkg/3ds/retroarch/filters/video
-		cp -v $WORK/$RADIR/gfx/video/video_filters/*.filt $WORK/$RADIR/pkg/3ds/retroarch/filters/video/
-		cp -v $WORK/$RADIR/gfx/audio/audio_filters/*.dsp $WORK/$RADIR/pkg/3ds/retroarch/filters/audio/
+		cp -v $WORK/$RADIR/gfx/video_filters/*.filt $WORK/$RADIR/pkg/3ds/retroarch/filters/video/
+		cp -v $WORK/$RADIR/audio/audio_filters/*.dsp $WORK/$RADIR/pkg/3ds/retroarch/filters/audio/
 		cp -v $RARCH_DIST_DIR/../info/*.info $WORK/$RADIR/pkg/3ds/retroarch/info/
 	fi
 fi
