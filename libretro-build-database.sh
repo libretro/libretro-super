fe6f1506f426b1dba65d271ba56a65da1e4288c2
Rob Loach <robloach@gmail.com>
2017-01-07 22:57:38 -0500

    Update Famicom Disk System to Family Computer Disk System

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -225,7 +225,7 @@ build_libretro_databases() {
 	build_libretro_database "NEC - PC Engine - TurboGrafx 16" "rom.crc"
 	build_libretro_database "NEC - PC Engine SuperGrafx" "rom.crc"
 	build_libretro_database "NEC - PC-FX" "rom.crc"
-	build_libretro_database "Nintendo - Famicom Disk System" "rom.crc"
+	build_libretro_database "Nintendo - Family Computer Disk System" "rom.crc"
 	build_libretro_database "Nintendo - Game Boy" "rom.crc"
 	build_libretro_database "Nintendo - Game Boy Advance" "rom.crc"
 	build_libretro_database "Nintendo - Game Boy Advance (e-Cards)" "rom.crc"
