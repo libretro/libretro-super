81eba67cf875c3b6e38b5fdfbe18056e221b0199
aliaspider <aliaspider@gmail.com>
2017-04-11 18:32:30 +0100

    (msvc) remove unneeded logfile.

--- retroarch-build-msvc2010-x64.bat
+++ retroarch-build-msvc2010-x64.bat
@@ -9,8 +9,8 @@ rem call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" 
 echo "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
-echo msbuild RetroArch-msvc2010.sln /p:configuration=Release;platform=x64 /flp:LogFile=buildlog.txt;verbosity=normal;Append
-call msbuild RetroArch-msvc2010.sln /p:configuration=Release;platform=x64 /flp:LogFile=buildlog.txt;verbosity=normal;Append
+echo msbuild RetroArch-msvc2010.sln /p:configuration=Release;platform=x64
+call msbuild RetroArch-msvc2010.sln /p:configuration=Release;platform=x64
 
 echo move x64\Release\RetroArch-msvc2010.exe ../../retroarch.exe
 move x64\Release\RetroArch-msvc2010.exe ../../retroarch.exe
--- retroarch-debug-build-msvc2010-x86.bat
+++ retroarch-debug-build-msvc2010-x86.bat
@@ -6,12 +6,13 @@ cd pkg\msvc
 echo "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 
-echo msbuild RetroArch-msvc2010.sln /p:configuration=Debug;platform=win32 /flp:LogFile=buildlog.txt;verbosity=normal;Append
-call msbuild RetroArch-msvc2010.sln /p:configuration=Debug;platform=win32 /flp:LogFile=buildlog.txt;verbosity=normal;Append
+echo msbuild RetroArch-msvc2010.sln /p:configuration=Debug;platform=win32
+call msbuild RetroArch-msvc2010.sln /p:configuration=Debug;platform=win32
 
 echo move Debug\RetroArch-msvc2010.exe ../../retroarch.exe
 move Debug\RetroArch-msvc2010.exe ../../retroarch.exe
 
 echo move Debug\RetroArch-msvc2010.exe.intermediate.manifest ../../retroarch.exe.manifest
 move Debug\RetroArch-msvc2010.exe.intermediate.manifest ../../retroarch.exe.manifest
+
 echo Build finished.
