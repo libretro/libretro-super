4fa3d91b68e8ca38ddcbd809069668d0386d1e7c
twinaphex <libretro@gmail.com>
2017-09-15 15:16:23 +0200

    Comment out currently non-building cores for XDK1/360

--- libretro-build-xdk360.sh
+++ libretro-build-xdk360.sh
@@ -37,26 +37,26 @@ if [ $1 ]; then
 	$1
 else
 	#build_libretro_beetle_lynx
-	build_libretro_beetle_gba
-	build_libretro_beetle_ngp
-	build_libretro_beetle_pce_fast
-	build_libretro_beetle_supergrafx
-	build_libretro_beetle_pcfx
-	build_libretro_mednafen_psx
-	build_libretro_beetle_vb
-	build_libretro_beetle_wswan
+	#build_libretro_beetle_gba
+	#build_libretro_beetle_ngp
+	#build_libretro_beetle_pce_fast
+	#build_libretro_beetle_supergrafx
+	#build_libretro_beetle_pcfx
+	#build_libretro_mednafen_psx
+	#build_libretro_beetle_vb
+	#build_libretro_beetle_wswan
 	#build_libretro_beetle_bsnes
-	build_libretro_snes9x2010
-	build_libretro_genesis_plus_gx
-	build_libretro_fb_alpha
+	#build_libretro_snes9x2010
+	#build_libretro_genesis_plus_gx
+	#build_libretro_fb_alpha
 	build_libretro_vba_next
 	build_libretro_fceumm
-	build_libretro_gambatte
-	build_libretro_nx
-	build_libretro_prboom
+	#build_libretro_gambatte
+	#build_libretro_nx
+	#build_libretro_prboom
 	#build_libretro_stella
 	#build_libretro_quicknes
 	build_libretro_nestopia
-	build_libretro_tyrquake
-	build_libretro_mame078
+	#build_libretro_tyrquake
+	#build_libretro_mame078
 fi
