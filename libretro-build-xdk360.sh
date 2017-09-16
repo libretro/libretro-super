08b07ea463a6edd977dad3ecdd7d5af63af226a2
twinaphex <libretro@gmail.com>
2017-09-16 07:08:43 +0200

    Update

--- libretro-build-xdk360.sh
+++ libretro-build-xdk360.sh
@@ -38,7 +38,7 @@ if [ $1 ]; then
 else
 	#build_libretro_beetle_lynx
 	#build_libretro_beetle_gba
-	#build_libretro_beetle_ngp
+	build_libretro_beetle_ngp
 	#build_libretro_beetle_pce_fast
 	#build_libretro_beetle_supergrafx
 	#build_libretro_beetle_pcfx
