4dcf393ee6cd81eefd5f81266843b3d918b0e501
Brad Parker <cbparker@gmail.com>
2017-10-21 15:05:49 -0400

    travis: add wii build script

---
+++ travis/build-wii.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RECIPE=recipes/nintendo/wii
+
+sudo mkdir -p /home/buildbot/tools
+
+sudo chmod -R 777 /home/buildbot
+
+cd /home/buildbot/tools
+
+# wiiu tools work for wii also
+wget -O wiiu.tar.xz 'https://github.com/libretro/libretro-toolchains/blob/master/wiiu.tar.xz?raw=true'
+
+tar Jkxf wiiu.tar.xz
+
+cd ~/libretro-super
+
+# only build the one core specified in $CORE
+egrep "^$CORE " ${RECIPE} | head -1 >${RECIPE}.new && mv ${RECIPE}.new ${RECIPE}
+
+./libretro-buildbot-recipe.sh ${RECIPE}
