7e0ef02ddd8525ebd07497b36431ef09393e90b7
Brad Parker <cbparker@gmail.com>
2017-10-31 18:02:16 -0400

    use new option to specify single core

--- travis/build-linux_x64.sh
+++ travis/build-linux_x64.sh
@@ -5,6 +5,4 @@ RECIPE=recipes/linux/cores-linux-x64-generic
 cd ~/libretro-super
 
 # only build the one core specified in $CORE
-egrep "^$CORE " ${RECIPE} | head -1 >${RECIPE}.new && mv ${RECIPE}.new ${RECIPE}
-
-./libretro-buildbot-recipe.sh ${RECIPE}
+SINGLE_CORE=${CORE} ./libretro-buildbot-recipe.sh ${RECIPE}
--- travis/build-wii.sh
+++ travis/build-wii.sh
@@ -16,6 +16,4 @@ tar Jkxf wiiu.tar.xz
 cd ~/libretro-super
 
 # only build the one core specified in $CORE
-egrep "^$CORE " ${RECIPE} | head -1 >${RECIPE}.new && mv ${RECIPE}.new ${RECIPE}
-
-./libretro-buildbot-recipe.sh ${RECIPE}
+SINGLE_CORE=${CORE} ./libretro-buildbot-recipe.sh ${RECIPE}
