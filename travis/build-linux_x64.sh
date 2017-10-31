b2762b91515d0aa386f035dbdd5691fc60e3b276
Brad Parker <cbparker@gmail.com>
2017-10-31 18:09:39 -0400

    travis: move build dir to where libretro-super would clone the repo, fixes building of PRs instead of upstream master

--- travis/build-linux_x64.sh
+++ travis/build-linux_x64.sh
@@ -4,5 +4,10 @@ RECIPE=recipes/linux/cores-linux-x64-generic
 
 cd ~/libretro-super
 
+if [ "${TRAVIS_BUILD_DIR}" ]; then
+  CORE_DIRNAME=`grep ${CORE} ${RECIPE} | head -1 | awk '{print $2}'`
+  mv ${TRAVIS_BUILD_DIR} ${CORE_DIRNAME}
+fi
+
 # only build the one core specified in $CORE
 SINGLE_CORE=${CORE} ./libretro-buildbot-recipe.sh ${RECIPE}
--- travis/build-ngc.sh
+++ travis/build-ngc.sh
@@ -15,5 +15,10 @@ tar Jkxf wiiu.tar.xz
 
 cd ~/libretro-super
 
+if [ "${TRAVIS_BUILD_DIR}" ]; then
+  CORE_DIRNAME=`grep ${CORE} ${RECIPE} | head -1 | awk '{print $2}'`
+  mv ${TRAVIS_BUILD_DIR} ${CORE_DIRNAME}
+fi
+
 # only build the one core specified in $CORE
 SINGLE_CORE=${CORE} ./libretro-buildbot-recipe.sh ${RECIPE}
