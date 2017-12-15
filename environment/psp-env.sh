fd87ec04eddd0b7215628cd7a61b945c3bd14a57
radius <andres.430@gmail.com>
2017-12-15 17:48:36 -0500

    update psp script

--- environment/psp-env.sh
+++ environment/psp-env.sh
@@ -1,17 +1,29 @@
 display_usage() { 
    echo -e "\nSetup a RetroArch PSP build environment on Debian/Ubuntu or MSYS2 (MINGW64 only)" 
-	echo -e "\nUsage: [install] [build] [export]\n" 
-	echo -e "It will install the toolchain in /home/buildbot/tools\n"
-	echo -e "Arguments:\n"
-	echo -e "install:\n install or re(install) the toolchain"
-	echo -e "build:\n update the source tree and build everything"
-	echo -e "export:\n setup the environment for local building, run with source psp-env.sh"
+   echo -e "\nUsage: [install] [build] [export]\n" 
+   echo -e "It will install the toolchain in /home/buildbot/tools\n"
+   echo -e "Arguments:\n"
+   echo -e "install:\n install or re(install) the toolchain"
+   echo -e "build:\n update the source tree and build everything"
+   echo -e "export:\n setup the environment for local building, run with source psp-env.sh"
 } 
 
+fetch()
+{
+   cd ~
+   if [ ! -d ~/libretro/psp ]; then
+      mkdir libretro
+      cd libretro
+      git clone https://github.com/libretro/libretro-super.git psp
+   fi
+}
+
 #!/bin/bash
+platform=psp1
 
 if [ "$1" = "install" ]; then
    if [[ "$MSYSTEM" == *"MINGW64"* ]]; then
+
       pacman -S git make p7zip tar wget
 
       mkdir -p /home/buildbot/tools/devkitpro
@@ -20,13 +32,10 @@ if [ "$1" = "install" ]; then
       tar zxvf devkitPSP_r16-1-x86_64-win.tar.gz
       rm devkitPSP_r16-1-x86_64-win.tar.gz
 
-      cd ~
-      mkdir libretro
-      cd libretro
-      git clone https://github.com/libretro/libretro-super.git psp
-
+      fetch 
    else
-      apt install build-essential git p7zip tar wget
+
+      sudo apt install build-essential git p7zip tar wget
 
       mkdir -p tools/devkitpro
       cd tools/devkitpro
@@ -34,34 +43,41 @@ if [ "$1" = "install" ]; then
       tar jxvf devkitPSP_r16-1-x86_64-linux.tar.bz2
       rm devkitPSP_r16-1-x86_64-linux.tar.bz2
 
-      cd ~
-      mkdir libretro
-      cd libretro
-      git clone https://github.com/libretro/libretro-super.git psp
+      fetch 
 
       cd ~
-      sudo mkdir -p /home/buildbot
-      sudo ln -s ~/tools /home/buildbot/tools
 
+      if [ ! -d "/home/buildbot/tools" ]; then
+         sudo mkdir -p /home/buildbot
+         sudo ln -s ~/tools /home/buildbot/tools
+      fi;
    fi;
+   echo $platform environment ready...
 fi;
 
 if [ "$1" = "build" ]; then
-   cd ~/libretro/psp
-   git pull
-   ./libretro-buildbot-recipe.sh recipes/playstation/psp
+   if [ -d "/home/buildbot/tools/devkitpro/" ]; then
+      cd ~/libretro/psp
+      git pull
+      ./libretro-buildbot-recipe.sh recipes/playstation/psp
+   else
+      echo $platform environment not found, run with install again...
+   fi
 fi;
 
 if [ "$1" = "export" ]; then
-   export PATH=$PATH:/home/buildbot/tools/devkitpro/devkitPSP/bin/
-   export DEVKITPRO=/home/buildbot/tools/devkitpro/
-   export DEVKITPSP=/home/buildbot/tools/devkitpro/devkitPSP/
-   export platform=psp1
-   export PLATFORM=psp1
-   export CC=psp-gcc
-   export CXX=psp-g++
-
-   echo environment ready...
+   if [ -d "/home/buildbot/tools/devkitpro/" ]; then
+      export PATH=$PATH:/home/buildbot/tools/devkitpro/devkitPSP/bin/
+      export DEVKITPRO=/home/buildbot/tools/devkitpro/
+      export DEVKITPSP=/home/buildbot/tools/devkitpro/devkitPSP/
+      export platform=psp1
+      export PLATFORM=psp1
+      export CC=psp-gcc
+      export CXX=psp-g++
+      echo $platform environment ready...
+   else
+      echo $platform environment not found, run with install again...
+   fi;
 fi
 
 if [ $# -le 0 ]; then 
