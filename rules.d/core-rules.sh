77544264409ef51a922cdf41a700a2f6811a3d9e
twinaphex <libretro@gmail.com>
2018-01-09 15:36:18 +0100

    Update fetch rule

--- rules.d/core-rules.sh
+++ rules.d/core-rules.sh
@@ -146,7 +146,7 @@ include_core_redream() {
 	register_module core "redream"
 }
 libretro_redream_name="Redream"
-libretro_redream_git_url="https://github.com/inolen/redream.git"
+libretro_redream_git_url="https://github.com/libretro/redream.git"
 libretro_redream_build_makefile="Makefile"
 libretro_redream_build_subdir="deps/libretro"
 
