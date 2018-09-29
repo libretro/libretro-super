bab47ca447b530ea1854c1c1378c20f94f6bc812
radius <andres.430@gmail.com>
2018-09-29 13:09:52 -0500

    update switch package

--- libretro-buildbot-recipe.sh
+++ libretro-buildbot-recipe.sh
@@ -1176,12 +1176,14 @@ if [ "${PLATFORM}" == "libnx" ] && [ "${RA}" == "YES" ]; then
 		cd $WORK/$RADIR
 		cp retroarch.cfg retroarch.default.cfg
 		mkdir -p pkg/libnx/retroarch/assets
+		mkdir -p pkg/libnx/retroarch/autoconfig
 		mkdir -p pkg/libnx/retroarch/cheats
 		mkdir -p pkg/libnx/retroarch/info
 		mkdir -p pkg/libnx/retroarch/overlays
 		mkdir -p pkg/libnx/retroarch/remaps
 		mkdir -p pkg/libnx/retroarch/shaders
 		cp -rf media/assets/* pkg/libnx/retroarch/assets
+		cp -rf media/autoconfig/* pkg/libnx/retroarch/autoconfig
 		cp -rf media/libretrodb/* pkg/libnx/retroarch/database
 		cp -rf media/overlays/* pkg/libnx/retroarch/overlays
 		cp -rf media/shaders/* pkg/libnx/retroarch/shaders
