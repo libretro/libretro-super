840ab4df122b8ac9c43412e09bcaa2fcf36ae8ff
Rob Loach <robloach@gmail.com>
2018-11-21 10:30:17 -0800

    player-rules: Add --recursive to post-fetch git
    Using --recursive will checkout all submodules of submodules.

--- rules.d/player-rules.sh
+++ rules.d/player-rules.sh
@@ -4,4 +4,4 @@ register_module player retroarch
 libretro_retroarch_name="RetroArch"
 libretro_retroarch_dir="retroarch"
 libretro_retroarch_git_url="https://github.com/libretro/RetroArch.git"
-libretro_retroarch_post_fetch_cmd="git submodule update --init"
+libretro_retroarch_post_fetch_cmd="git submodule update --init --recursive"
