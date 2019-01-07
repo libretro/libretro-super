70b7cddaf1de6da90cb6de5c22e9a0d42194a6a5
markwkidd <mark@stardart.net>
2019-01-07 12:19:05 -0500

    remove non-existent MAME 2003-Plus - Midway db

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -326,7 +326,6 @@ build_libretro_databases() {
 	build_libretro_database "MAME" "rom.crc"
 	build_libretro_database "MAME 2000" "rom.crc"
 	build_libretro_database "MAME 2003" "rom.crc"
-	build_libretro_database "MAME 2003 (Midway)" "rom.crc"
 	build_libretro_database "MAME 2003-Plus" "rom.crc"
 	build_libretro_database "MAME 2010" "rom.crc"
 	build_libretro_database "MAME 2014" "rom.crc"
