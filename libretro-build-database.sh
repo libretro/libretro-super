dc3b9ca79b07c0f78b64d4eb8412fd0b86d6fb98
Rob Loach <robloach@gmail.com>
2019-03-25 23:25:54 -0400

    db: Add Commodore Plus-4 and VIC-20
    @zach-morris found that Commodore Plus-4 and VIC-20 were missing:
    https://github.com/RobLoach/libretro-thumbnails-check/issues/16

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -251,6 +251,8 @@ build_libretro_databases() {
 	build_libretro_database "Coleco - ColecoVision" "rom.crc"
 	build_libretro_database "Commodore - 64" "rom.crc"
 	build_libretro_database "Commodore - Amiga" "rom.crc"
+	build_libretro_database "Commodore - Plus-4" "rom.crc"
+	build_libretro_database "Commodore - VIC-20" "rom.crc"
 	build_libretro_database "Dinothawr" "rom.crc"
 	build_libretro_database "Emerson - Arcadia 2001" "rom.crc"
 	build_libretro_database "Entex - Adventure Vision" "rom.crc"
