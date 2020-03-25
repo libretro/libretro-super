732d52d01a3d733f97264fbbda1ee5b503dbc617
Vladimir Serbinenko <phcoder@google.com>
2020-03-25 11:39:16 +0100

    Fix derivation of psp1 sdk path

--- cmake/psp1.cmake
+++ cmake/psp1.cmake
@@ -16,7 +16,7 @@ set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
 set(CMAKE_C_LINK_FLAGS "")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-execute_process(COMMAND psp-config --pspsdk-path OUTPUT_VARIABLE PSPSDK_PATH)
+execute_process(COMMAND psp-config --pspsdk-path OUTPUT_VARIABLE PSPSDK_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 set(CMAKE_CXX_FLAGS " -I${PSPSDK_PATH}/include -G0 -Wcast-align")
 set(CMAKE_C_FLAGS " -I${PSPSDK_PATH}/include -G0 -Wcast-align")
