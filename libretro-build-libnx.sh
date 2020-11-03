57c1d4d02dee2ee7f17f03d2f81fe854eff72fe8
Mitchell Lehman <lehmml04@pfw.edu>
2020-11-03 10:41:16 -0500

    Add cores from recipe to libnx build script

---
+++ libretro-build-libnx.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+# vim: set ts=3 sw=3 noet ft=sh : bash
+
+SCRIPT="${0#./}"
+BASE_DIR="${SCRIPT%/*}"
+WORKDIR="$PWD"
+
+if [ "$BASE_DIR" = "$SCRIPT" ]; then
+	BASE_DIR="$WORKDIR"
+else
+	if [[ "$0" != /* ]]; then
+		# Make the path absolute
+		BASE_DIR="$WORKDIR/$BASE_DIR"
+	fi
+fi
+
+if [[ -z "$1" ]]; then
+WANT_CORES=$(grep -Eo '^[^ ]+' ${BASE_DIR}/recipes/nintendo/libnx)
+
+else
+WANT_CORES="$@"
+fi
+
+platform=libnx ${BASE_DIR}/libretro-build.sh ${WANT_CORES}
