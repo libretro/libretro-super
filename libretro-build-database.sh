25d61a376f58a1a0deded68c4b6323096f9b5b6d
Rob Loach <robloach@gmail.com>
2021-12-09 07:19:52 -0500

    database: Add Amstrad GX4000 database
    Somehow we weren't considering GX4000, even though Caprice can run it. TOSEC provides a database for it, which I'll be able to build next cycle.

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -239,6 +239,7 @@ build_libretro_database() {
 
 build_libretro_databases() {
 	build_libretro_database "Amstrad - CPC" "rom.crc"
+	build_libretro_database "Amstrad - GX4000" "rom.crc"
 	build_libretro_database "Mattel - Intellivision" "rom.crc"
 	build_libretro_database "ScummVM" "rom.crc"
 	build_libretro_database "DOS" "rom.crc"
