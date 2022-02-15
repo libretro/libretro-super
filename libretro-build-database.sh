57ae0c3ea015e5a477ccf7f4bfd657fa70e927ab
Rob Loach <robloach@gmail.com>
2022-02-15 13:36:09 -0500

    database: Build the WASM-4 database (#1614)
    https://github.com/libretro/libretro-database/blob/master/dat/WASM-4.dat

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -355,6 +355,7 @@ build_libretro_databases() {
 	build_libretro_database "Quake III" "rom.crc"
 	build_libretro_database "RPG Maker" "rom.crc"
 	build_libretro_database "Flashback" "rom.crc"
+	build_libretro_database "WASM-4" "rom.crc"
 	build_libretro_database "Wolfenstein 3D" "rom.crc"
 }
 
