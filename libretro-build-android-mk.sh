7f81e8cb8cac8c54e346140489f86c39022858f3
Giovanni Cascione <ing.cascione@gmail.com>
2023-03-05 17:32:39 +0100

    Update as per official scummvm libretro port

--- libretro-build-android-mk.sh
+++ libretro-build-android-mk.sh
@@ -195,7 +195,7 @@ for core in $WANT_CORES; do
 	fi
 	
 	if [ $core = "scummvm" ]; then
-		path="./jni"
+		path="backends/platform/libretro/build/jni"
 	fi
 	if [ $core = "lowresnx" ]; then
 		path="platform/LibRetro/jni"
