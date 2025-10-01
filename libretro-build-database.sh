57c53305b755e3e1282ed476461db892ab6875e9
zoltanvb <balogh.zoltan.v@gmail.com>
2025-10-01 22:10:22 +0200

    Add Videoton TV-Computer database support via ep128emu.

--- libretro-build-database.sh
+++ libretro-build-database.sh
@@ -383,6 +383,7 @@ build_libretro_databases() {
 	build_libretro_database "MicroW8" "rom.crc"
 	build_libretro_database "Spectravideo - SVI-318 - SVI-328" "rom.crc"
 	build_libretro_database "PICO-8" "rom.crc"
+	build_libretro_database "Videoton - TV-Computer" "rom.crc"
 }
 
 build_libretrodb
